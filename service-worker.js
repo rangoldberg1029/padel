0d16c496b60c3ae241d079399eade114c130bcc6
rangoldberg1029 <92925015+rangoldberg1029@users.noreply.github.com>
2025-08-15 22:34:36 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Service worker v12: force update + auto-activate
-const CACHE = "padel-buddy-v12";
+// Service worker v14: force update + auto-activate
+const CACHE = "padel-buddy-v14";
 const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",(e)=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",(e)=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))); await self.clients.claim(); })());});
